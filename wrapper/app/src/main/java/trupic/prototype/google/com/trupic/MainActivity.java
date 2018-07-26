0129351d052b4c55ea1b489f620b73fd946229c6
Khairul Nizam <nizam@mediacorp.com.sg>
2018-07-26 17:36:05 +0800

    Wrapper files
    Signed-off-by: Khairul Nizam <nizam@mediacorp.com.sg>

---
+++ wrapper/app/src/main/java/trupic/prototype/google/com/trupic/MainActivity.java
@@ -0,0 +1,35 @@
+package trupic.prototype.google.com.trupic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import im.delight.android.webview.AdvancedWebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    AdvancedWebView wv_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv_view = this.findViewById(R.id.wv_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        WebSettings settings = wv_view.getSettings();
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(false);
+        wv_view.loadUrl("http://trustedmedia2018.appspot.com/");
+    }
+}
