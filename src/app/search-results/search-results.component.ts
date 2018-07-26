b45e4e8824c42d8f08f32c0c6f9011bcd4e5fcdb
Sergio Noviello <sergio.noviello@storyful.com>
2018-07-26 15:19:55 +0800

    style

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -5,7 +5,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.css']
+  styleUrls: ['./search-results.component.scss']
 })
 export class SearchResultsComponent implements OnInit {
   results;
