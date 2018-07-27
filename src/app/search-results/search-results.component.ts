918cfab1ca83062da3525f407034cfe2dbc38877
Sergio Noviello <sergio.noviello@storyful.com>
2018-07-27 11:09:26 +0800

    translate api

--- src/app/search-results/search-results.component.ts
+++ src/app/search-results/search-results.component.ts
@@ -16,20 +16,29 @@ export class SearchResultsComponent implements OnInit {
   filterField: string;
   range: string;
   selectedLanguage: string;
+  selectedToLanguage: string;
+  languages;
+  fromLanguage: string;
+  toLanguage: string;
+  translatedText: string;
   constructor(private imageSearchService: ImageSearchService, public _DomSanitizationService: DomSanitizer, private translateService: TranslateService) { }
 
   ngOnInit() {
     this.results = this.imageSearchService.searchResults();
+    this.translatedText = '';
     this.orderBy = 'Oldest';
     this.order = 'unix_time';
     this.ascending = true;
     this.filterField = 'unix_time';
     this.range = 'Any time';
     this.selectedLanguage = 'No language selected';
-
+    this.selectedToLanguage = 'No language selected';
+    this.languages = [];
+    this.fromLanguage = '';
+    this.toLanguage = '';
     this.translateService.getLanguageList().subscribe(res => {
-      console.log('languages', res);
       this.translateService.languages = res;
+      this.languages = res;
     }, error => {
 
     });
@@ -49,7 +58,19 @@ export class SearchResultsComponent implements OnInit {
     this.range = ev.target.innerText;
   }
 
-  changeLanguage(ev) {
-    this.selectedLanguage = ev.target.innerText;
+  changeLanguage(lang) {
+    this.selectedLanguage = lang.label;
+    this.fromLanguage = lang.code;
+  }
+
+  translateTo(lang) {
+    this.selectedToLanguage = lang.label;
+    this.toLanguage = lang.code;
+
+    this.translateService.translate(this.results.image_text, this.fromLanguage, this.toLanguage).subscribe(res => {
+      this.translatedText = res;
+    }, error => {
+      this.translatedText = 'error';
+    } );
   }
 }
